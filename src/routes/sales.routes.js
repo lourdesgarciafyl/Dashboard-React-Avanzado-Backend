1d66d0499e0a1eff563cbb494013ac84456bc848
Nicolas Herrera <herreranicoo17@gmail.com>
2023-10-16 20:46:25 -0300

    Se agrego ruta y controlador para obtener las ventas por fecha

--- src/routes/sales.routes.js
+++ src/routes/sales.routes.js
@@ -1,11 +1,19 @@
 import { Router } from "express";
 import validateSale from "../helpers/validateSale";
-import { cancelSale, createSale, deleteSale, getListSales, getSale } from "../controllers/sales.controllers";
+import {
+  cancelSale,
+  createSale,
+  deleteSale,
+  getListSales,
+  getSale,
+  getSalesPerDay,
+} from "../controllers/sales.controllers";
 import validateJWT from "../helpers/validateToken";
 
 const router = Router();
-router.route("/").post(validateSale, createSale).get(validateJWT, getListSales)
-router.route("/:id").get(validateJWT,getSale).delete(validateJWT, deleteSale)
-router.route("/cancelsale/:id").put(validateJWT, cancelSale)
+router.route("/").post(validateSale, createSale).get(validateJWT, getListSales);
+router.route("/:id").get(validateJWT, getSale).delete(validateJWT, deleteSale);
+router.route("/cancelsale/:id").put(validateJWT, cancelSale);
+router.route("/filteredSales/:day").get(getSalesPerDay);
 
-export default router
+export default router;
